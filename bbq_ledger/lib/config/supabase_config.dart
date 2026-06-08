776116c45dd396595e57279eb553cdb318fad2b9
mudrock-arknight <mudrock-arknight@users.noreply.github.com>
2026-06-08 01:00:13 +0000

    feat: 开发客户账本APK
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- bbq_ledger/lib/config/supabase_config.dart
+++ bbq_ledger/lib/config/supabase_config.dart
@@ -2,6 +2,6 @@
 class SupabaseConfig {
   // TODO: 替换为你的 Supabase 项目 URL 和 anon key
   // 后期迁移到自托管时只需修改这两个值
-  static const String url = 'YOUR_SUPABASE_URL';
-  static const String anonKey = 'YOUR_SUPABASE_ANON_KEY';
+  static const String url = 'https://wtoukdzykvuoycrolpfq.supabase.co';
+  static const String anonKey = 'sb_publishable_B5lYVZB84nUexaNwYjOekw_JJiikMpL';
 }
